ea713624cb662ed53856c4a3ff1274444665c60f
Sunny-117 <1669248141@qq.com>
2023-11-04 21:37:03 +0800

    feat: client basic

---
+++ client.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+type Client struct {
+	ServerIp   string
+	ServerPort int
+	Name       string
+	conn       net.Conn
+}
+
+func NewClient(serverIp string, serverPort int) *Client {
+	// create client
+	client := &Client{
+		ServerIp:   serverIp,
+		ServerPort: serverPort,
+	}
+	// connect server
+	conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", serverIp, serverPort))
+	if err != nil {
+		fmt.Println("net.Dial error: ", err)
+		return nil
+	}
+
+	client.conn = conn
+
+	return client
+}
+
+func main() {
+	client := NewClient("127.0.0.1", 8888)
+	if client == nil {
+		fmt.Println("fail to connect client")
+		return
+	}
+	fmt.Println("connect success")
+
+	select {}
+}
